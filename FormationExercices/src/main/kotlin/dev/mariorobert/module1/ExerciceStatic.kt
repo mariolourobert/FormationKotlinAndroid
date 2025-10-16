a411cadb89b8ab0c795740d972efdd18752d8c4f
Mario ROBERT <mariolourobert@gmail.com>
2025-10-16 22:19:20 +0200

    ExerciceStatic

---
+++ FormationExercices/src/main/kotlin/dev/mariorobert/module1/ExerciceStatic.kt
@@ -0,0 +1,22 @@
+package dev.mariorobert.module1
+
+/**
+ * Step 1 :
+ * Écrivez une "classe" Logger qui permet de stocker des logs (des String).
+ * Logger doit avoir deux fonctions :
+ * - addLog(message: String) : ajoute un log (mais ne print rien)
+ * - printLogs() : print tous les logs
+ *
+ * Logger doit stocker les logs en interne.
+ *
+ * On veut pouvoir utiliser Logger sans avoir à l'instancier.
+ *
+ * Utiliser le String Template pour formater les logs.
+ * Exemple : logs = "$logs$message\n"
+ *
+ * Step 2 :
+ * Utilisez Logger dans votre [MessageValidator] de l'exercice précédent pour logguer chaque validation.
+ * Logguez le message à valider, le mode (strict ou non) et le résultat de la validation.
+ */
+fun main(args: Array<String>) {
+}
