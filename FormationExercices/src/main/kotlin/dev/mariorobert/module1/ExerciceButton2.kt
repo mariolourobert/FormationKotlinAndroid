b7dc8991a0c20ad2a917ffe058ac9a7455ddfa4d
Mario ROBERT <mariolourobert@gmail.com>
2025-10-18 00:06:08 +0200

    ExerciceButton2

---
+++ FormationExercices/src/main/kotlin/dev/mariorobert/module1/ExerciceButton2.kt
@@ -0,0 +1,15 @@
+package dev.mariorobert.module1
+
+
+/**
+ * Copiez la classe Button de l'ExerciceButton, ainsi que la fonction showButton.
+ * Commentez ou supprimez les de l'ExerciceButton pour éviter les conflits.
+ *
+ * Step 1 :
+ * On veut que le paramètre onClick de showButton soit optionnel.
+ *
+ * Step 2 :
+ * L'initialisation de Button dans showButton peut être améliorée...
+ */
+fun main(args: Array<String>) {
+}
