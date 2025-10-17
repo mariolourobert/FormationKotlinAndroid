43a96ffb83ddf69c938b62ba59d8bbeeade9449a
Mario ROBERT <mariolourobert@gmail.com>
2025-10-17 18:13:52 +0200

    ExerciceButton

---
+++ FormationExercices/src/main/kotlin/dev/mariorobert/module1/ExerciceButton.kt
@@ -0,0 +1,22 @@
+package dev.mariorobert.module1
+
+
+/**
+ * Step 1 :
+ * Créez une classe Button avec deux fonctions :
+ * - setTitle : qui prend en paramètre une String et print le titre
+ * - setOnClickListener : qui prend en paramètre une lambda très simple et print "Button click listener set."
+ * Utilisez la classe Button dans la fonction main.
+ *
+ * Step 2 :
+ * Écrivez une fonction showButton qui prendra les paramètres nécessaires pour créer un Button avec un titre
+ * et un listener.
+ * Dans la fonction main, remplacez le code de création du Button par un appel à showButton.
+ *
+ * Step 3 :
+ * Ajouter un print dans votre lambda. Est-il printé lors de l'appel à showButton ?
+ * Faites en sorte que le print soit exécuté.
+ */
+fun main(args: Array<String>) {
+
+}
