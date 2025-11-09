596283b5429bb24b72f21c3593d84beb427889fb
Mario ROBERT <mariolourobert@gmail.com>
2025-11-09 21:51:48 +0100

    Solution - Exercice - ViewBinding

--- FormationViews/app/src/main/java/dev/mariorobert/formationviews/MainActivity.kt
+++ FormationViews/app/src/main/java/dev/mariorobert/formationviews/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.mariorobert.formationviews
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dev.mariorobert.formationviews.databinding.ActivityMainBinding
 
@@ -13,5 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.submit.setOnClickListener {
+            Log.d("MainActivity", binding.name.text.toString())
+        }
+
+        binding.clear.setOnClickListener {
+            binding.name.setText("")
+        }
     }
 }
