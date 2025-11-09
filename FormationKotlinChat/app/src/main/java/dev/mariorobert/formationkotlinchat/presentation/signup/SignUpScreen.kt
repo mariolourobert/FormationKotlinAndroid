f00b5ed99b550a1907227ab25c4b6c5f15f1f640
Mario ROBERT <mariolourobert@gmail.com>
2025-11-09 22:51:05 +0100

    Solution - Exercice - SignUpScreen - Improvements

--- FormationKotlinChat/app/src/main/java/dev/mariorobert/formationkotlinchat/presentation/signup/SignUpScreen.kt
+++ FormationKotlinChat/app/src/main/java/dev/mariorobert/formationkotlinchat/presentation/signup/SignUpScreen.kt
@@ -1,50 +1,97 @@
 package dev.mariorobert.formationkotlinchat.presentation.signup
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SignUpScreen() {
     Column(
         horizontalAlignment = Alignment.End,
+        modifier = Modifier.fillMaxSize(),
     ) {
-        TextField(
-            value = "",
-            onValueChange = {},
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text("Username") }
-        )
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .weight(1f)
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center,
         ) {
-            Checkbox(
-                checked = false,
-                onCheckedChange = {}
+            Icon(
+                modifier = Modifier.fillMaxSize(0.5f),
+                imageVector = Icons.Default.Send,
+                contentDescription = "Logo",
             )
-            Text("I agree to the Terms and Conditions")
         }
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .weight(2f)
+                .verticalScroll(rememberScrollState())
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Button(
-                onClick = { },
+            Text(
+                text = "Kotlin Chat",
+                fontSize = 32.sp,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 14.dp),
+                textAlign = TextAlign.Center,
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 14.dp)
+                    .padding(top = 16.dp),
+                placeholder = { Text("Enter your username") },
+                label = {
+                    Text("Username")
+                }
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text("Cancel")
+                Checkbox(
+                    checked = false,
+                    onCheckedChange = {}
+                )
+                Text("I agree to the Terms and Conditions")
             }
-            Button(
-                onClick = { },
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 14.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text("Sign Up")
+                Button(
+                    onClick = { },
+                ) {
+                    Text("Cancel")
+                }
+                Button(
+                    onClick = { },
+                ) {
+                    Text("Sign Up")
+                }
             }
         }
     }
