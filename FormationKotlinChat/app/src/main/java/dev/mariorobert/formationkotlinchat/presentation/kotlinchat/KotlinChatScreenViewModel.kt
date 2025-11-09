e0aea4adac94f98f716bfb65f921b564580f8b81
Mario ROBERT <mariolourobert@gmail.com>
2025-11-09 21:40:32 +0100

    Solution - Exercice - KotlinChatScreenViewModel

---
+++ FormationKotlinChat/app/src/main/java/dev/mariorobert/formationkotlinchat/presentation/kotlinchat/KotlinChatScreenViewModel.kt
@@ -0,0 +1,41 @@
+package dev.mariorobert.formationkotlinchat.presentation.kotlinchat
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.mariorobert.formationkotlinchat.data.MessagesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class KotlinChatScreenViewModel : ViewModel() {
+    private val repository = MessagesRepository()
+
+    val uiState = MutableStateFlow<KotlinChatScreenUiState>(
+        KotlinChatScreenUiState(
+            messages = emptyList(),
+        )
+    )
+
+    init {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.messages.collect {
+                val messagesAsString = it.map { messageDataModel ->
+                    messageDataModel.content
+                }
+                uiState.emit(
+                    KotlinChatScreenUiState(
+                        messages = messagesAsString,
+                    )
+                )
+            }
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            delay(3000)
+            repository.sendMessage(
+                authorName = "Charlie",
+                content = "Hey everyone, what's up?",
+            )
+        }
+    }
+}
