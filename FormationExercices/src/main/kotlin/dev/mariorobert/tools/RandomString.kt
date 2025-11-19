5a9cadb8c1d5b1e0b35f0bde40160b9f11f63f66
Mario ROBERT <mariolourobert@gmail.com>
2025-11-19 23:19:53 +0100

    Improve Random.nextString

--- FormationExercices/src/main/kotlin/dev/mariorobert/tools/RandomString.kt
+++ FormationExercices/src/main/kotlin/dev/mariorobert/tools/RandomString.kt
@@ -5,9 +5,15 @@ import kotlin.random.Random
 private val lowerCaseAlphabet: List<Char> = ('a'..'z').toList()
 private val upperCaseAlphabet: List<Char> = ('A'..'Z').toList()
 private val digits: List<Char> = ('1'..'9').toList()
+private val specialChars = listOf('@', '#', '$', '%', '^', '&', '*')
 
 fun Random.nextString(length: Int = nextInt(2, 20)): String {
-    val alphabet = lowerCaseAlphabet + upperCaseAlphabet + digits
+    val includeSpecialChars = nextBoolean()
+    val alphabet = if (includeSpecialChars) {
+        lowerCaseAlphabet + upperCaseAlphabet + digits + specialChars
+    } else {
+        lowerCaseAlphabet + upperCaseAlphabet + digits
+    }
 
     return (1..length)
         .map { alphabet.random() }
